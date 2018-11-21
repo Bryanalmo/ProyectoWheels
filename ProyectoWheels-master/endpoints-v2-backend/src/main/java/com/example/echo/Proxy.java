b2ab64bcd5aafaa0caf51efd526ebd0b4ada3513
amsoche <amsoche@192.168.1.115>
2018-11-21 17:38:30 -0500

    sesion v6

--- ProyectoWheels-master/endpoints-v2-backend/src/main/java/com/example/echo/Proxy.java
+++ ProyectoWheels-master/endpoints-v2-backend/src/main/java/com/example/echo/Proxy.java
@@ -1,5 +1,21 @@
 package com.example.echo;
 
+import java.util.ArrayList;
+
+import com.google.api.server.spi.auth.EspAuthenticator;
+import com.google.api.server.spi.auth.common.User;
+import com.google.api.server.spi.config.AnnotationBoolean;
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiIssuer;
+import com.google.api.server.spi.config.ApiIssuerAudience;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.config.Nullable;
+import com.google.api.server.spi.response.UnauthorizedException;
+
+import io.swagger.annotations.ApiParam;
+
 /**
  * The Echo API which Endpoints will be exposing.
  */
