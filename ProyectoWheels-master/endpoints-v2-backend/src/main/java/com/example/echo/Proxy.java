c3189cf506826ab8c22b46e0e4aaf2fdb5c37c2e
amsoche <amsoche@192.168.1.115>
2018-11-21 23:57:47 -0500

    neew

--- ProyectoWheels-master/endpoints-v2-backend/src/main/java/com/example/echo/Proxy.java
+++ ProyectoWheels-master/endpoints-v2-backend/src/main/java/com/example/echo/Proxy.java
@@ -88,7 +88,7 @@ public class Proxy implements IProxy {
     	user.setCorreo("123");
     	user.setPassword("123");
         if(user.getCorreo().equals(correo) && user.getPassword().equals(password) ){
-           
+           return user;
         }
         return null;
     }
